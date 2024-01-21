fed4893690c0622b0ca247fdc65741f27cb59ba8
Giovanni Vella <giovanni.vella98@gmail.com>
2024-01-21 11:45:18 +0100

    update default back end server port

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,7 @@ import * as path from "path";
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(cors({origin: '*'}));
